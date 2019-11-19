c682b3a7f2ac837029b78cd49f80484d4818b728
alexrpollock <arpollock@mines.edu>
2019-11-18 22:10:11 -0700

    Failed attempts to get get lake detection working

--- feature_matrices.py
+++ feature_matrices.py
@@ -2,6 +2,11 @@ import cv2
 import os
 import numpy as np
 import math
+import pandas as pd
+import shapefile as shp # pip3 install pyshp
+from return_pixel import return_pixel
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
 
 top_image = None
 ocean_image = None
@@ -22,8 +27,63 @@ LOWER_RIGHT_LONG = -51.55  # longitude of lower right corner of topo image
 MAP_LON_DELTA = LOWER_RIGHT_LONG - UPPER_LEFT_LONG
 MAP_LAT_BOTTOM_DEGREE = LOWER_RIGHT_LAT * math.pi / 180
 
+WATER_DF = None
+
 # https://databasin.org/maps/new#datasets=588bcc4e1f2646e589e0fc9593498e3d
 
+def check_lat(latitude=0) -> bool:
+    if latitude > UPPER_LEFT_LAT or latitude < LOWER_RIGHT_LAT:
+        # print(f'ERROR: invalid latitude value for current area.')
+        return False
+    else:
+        return True
+
+
+def check_long(longitude=0) -> bool:
+    if longitude < UPPER_LEFT_LONG and longitude > LOWER_RIGHT_LONG:
+        # print(f'ERROR: invalid longitude value for current area.')
+        return False
+    else:
+        return True
+
+def read_shapefile():
+    """
+    Read a shapefile into a Pandas dataframe with a 'coords'
+    column holding the geometry information. This uses the pyshp
+    package
+    """
+    shp_path = './data/ne10mlakes/ne_10m_lakes.shp'
+    sf = shp.Reader(shp_path,encoding="ISO-8859-1")
+
+    fields = [x[0] for x in sf.fields][1:]
+    records = sf.records()
+    shps = [s.points for s in sf.shapes()]
+    df = pd.DataFrame(columns=fields, data=records)
+    df = df.assign(coords=shps)
+    return df
+
+def read_water_data():
+    global WATER_DF
+    WATER_DF = read_shapefile()
+    WATER_DF.drop(axis=1, columns=['note', 'delta',	'dam_name', 'year', 'admin', 'name_abb', 'name_alt'], inplace=True)
+
+    WATER_DF["polygon"] = WATER_DF["coords"].apply(lambda coords: Polygon(coords))
+    all_pixels = []
+    for index, row in WATER_DF.iterrows():
+        # polygon = Polygon(row['coords'])
+        pixels = []
+        for coord in row['coords']:
+            if( check_lat(coord[0]) and check_long(coord[1]) ):
+                x, y = return_pixel(coord[0], coord[1])
+                pixels.append([x, y])
+                # print(coord)
+                # print(type(coord))
+        all_pixels.append(Polygon(pixels))
+    WATER_DF["pixel_coords"] = all_pixels
+        # print(' ')
+        # if(index > 5):
+        #     return
+    # print(WATER_DF.head())
 
 def read_image():
     """Uses OpenCV to read the various images and get the needed constants for
@@ -70,12 +130,34 @@ def in_bounds(x, y):
     else:
         return False
 
-def get_ocean_or_land(x, y):
+def lake_points(arr):
+    for index, row in WATER_DF.iterrows():
+        if row["pixel_coords"] and row["pixel_coords"].exterior:
+            # TODO: this does not work (working version took 6 years)
+            coords = list(row["pixel_coords"].exterior.coords)
+            # print(coords)
+            xs = [coord[0] for coord in coords]
+            ys = [coord[1] for coord in coords]
+            min_x = round(min(xs))
+            min_y = round(min(ys))
+            max_x = round(max(xs))
+            max_y = round(max(ys))
+            for x in range(min_x, max_x):
+                for y in range(min_y, max_y):
+                    if in_bounds(x, y) and row["pixel_coords"].contains(Point(x, y)):
+                        arr[y, x] = 2
+    return arr
+
+def get_water_or_land(x, y):
     ''' returns 0 for ocean (black pixel) or 1 for land (white),
     returns -1 if unknown'''
     if in_bounds(x, y):
         (b, g, r) = ocean_image[y, x]
         if b > 127 and g > 127 and r > 127:  # assume white -> land
+            # check if in lake
+            # for index, row in WATER_DF.iterrows():
+            #     if row["pixel_coords"].contains(Point(x, y)):
+            #         return 2
             return 1
         if b < 127 and g < 127 and r < 127:  # assume black -> ocean
             return 0
@@ -129,10 +211,12 @@ if __name__ == "__main__":
         if option == -1:
             quit()
         elif option == 0:
+            read_water_data()
             out_file = "ocean_or_land.npy"
             for y in range(h):
                 for x in range(w):
-                    out_arr[y,x] = get_ocean_or_land(x, y)
+                    out_arr[y,x] = get_water_or_land(x, y)
+            out_arr = lake_points(out_arr)
         elif option == 1:
             out_file = "elevation.npy"
             for y in range(h):
@@ -152,6 +236,6 @@ if __name__ == "__main__":
         else:
             print("INVALID OPTION. Please try again.")
             continue
-        if not option == -2:
+        if not option == -2 and not option == 2:
             np.save(out_file, out_arr)
         
